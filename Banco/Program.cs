dc79afa52d9d8601306a9af5d2d7751c074850b0
VPhyre <vinipaulnic@hotmail.com>
2018-10-13 00:22:23 -0300

    Finalizando correção

---
+++ Banco/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SistemaBanco Cliente1;
+            double deposito;
+            Console.WriteLine("Digite o Numero da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome do titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Cliente1 = new SistemaBanco(conta, nome);
+
+            Console.WriteLine("Haverá depósito inicial(s/n): ");
+            string Escolha = Console.ReadLine();
+
+
+            if (Escolha == "s")
+            {
+                Console.WriteLine("Digite o valor do deposito inicial: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Cliente1.FazerDeposito(deposito);
+
+            }
+
+
+            //Console.WriteLine("Conta criada!");
+            //Console.WriteLine("Conta: " + Cliente1.NumConta + ", Titular: " + Cliente1.Nome + ", Saldo: " + Cliente1.SaldoConta);
+
+            Console.WriteLine(Cliente1);
+
+            Console.WriteLine("Digite um valor para deposito: ");
+            deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Cliente1.FazerDeposito(deposito);
+
+            Console.WriteLine(Cliente1);
+
+            Console.WriteLine("Digite um valor para Saque: ");
+            Double Saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Cliente1.FazerSaque(Saque);
+
+            Console.WriteLine(Cliente1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Banco/SistemaBanco.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+namespace Banco
+{
+    class SistemaBanco
+    {
+        public int NumConta{ get; private set; }
+        public string Nome { get; set; }
+        public double Deposito { get; private set; }
+        public double SaldoConta { get; private set; }
+
+        //Construtor
+        public SistemaBanco(int NumConta, string Nome, double SaldoConta)
+        {
+
+            this.NumConta = NumConta;
+            this.Nome = Nome;
+            this.SaldoConta = SaldoConta;
+            
+        }
+
+        //Sobrecarga
+        public SistemaBanco(int NumConta, string Nome)
+        {
+
+            this.NumConta = NumConta;
+            this.Nome = Nome;
+            this.SaldoConta = 0;
+        }
+        //fazer deposito
+        public void FazerDeposito(double Valor)
+        {
+            SaldoConta += Valor;
+        }
+
+        //fazer saque
+        public void FazerSaque(double Valor)
+        {
+            SaldoConta -= (Valor + 5);
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta: "
+                +
+                NumConta
+                + ", Titular: "
+                + Nome
+                + " Saldo: $ "
+                + SaldoConta.ToString("F2", CultureInfo.InvariantCulture);
+                
+                
+
+
+        }
+    }
+}
